86f3c84e6a8ebe8561107c7a7560ca4c6035b84a
MyNameBeTaken <Andrew.kuo0130@gmail.com>
2025-04-14 14:50:44 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+function showMessage() {
+    console.log("here")
+    alert("Hello! Welcome to my website.");
+}
+
+showMessage()
+
+
+function changeBgColor1() {
+    document.body.style.backgroundImage = "url('EVA.jpg')"
+}
+
+function changeBgColor2() {
+    document.body.style.backgroundImage = "url('eva2.jpg')"
+}
+
+function changeBgColorblank() {
+    document.body.style.backgroundImage = "url('white.jpg')"
+}
+
+
+
+function enter() {
+    let name = prompt("enter password");
+    if (name != "I'm so handsome!") {
+        // <a herf="unnamed.webp"></a>
+    }
+
+}
+function enterage() {
+    let age = prompt("enter your age");
+    if (age >= 18) {
+        alert("you're an adult");
+    } else {
+        alert("You're still a kid")
+    }
+}
+
+
+function namechecker() {
+    let name = prompt("enter password");
+    if (name != "I'm so handsome!") {
+        downloadURL(files[unnamed.webp])
+    }
+}
+
+function agechecker() {
+    let age = prompt("enter your age");
+    if (age >= 18) {
+        alert("you're an adult");
+    } else {
+        alert("You're still a kid")
+    }
+}
+namechecker()
+agechecker()
+
